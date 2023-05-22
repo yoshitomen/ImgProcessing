34bfc58da0983cbb5f5f6922c5248f9c17ea58da
yoshitomen <y.f.m.aquar.phy@gmail.com>
2023-05-22 23:20:33 +0900

    change image size to small

--- hirasawa/photo/Resize_DelMeta.py
+++ hirasawa/photo/Resize_DelMeta.py
@@ -2,17 +2,17 @@
 import os
 from PIL import Image
 
-def resize():
+def resize(x,y):
     dir_name = "origin"
     #os.mkdir("resized")
-    resize_size = (6000,4000)
-    new_dir_name = "resized"
+    resize_size = (x,y)
+    resize_dir = "resized"
     files = os.listdir(dir_name)
     for file in files:
-            if file == '.DS_Store':continue #Mac特有の隠しファイル対策.
-            photo = Image.open(os.path.join(dir_name, file))
-            photo_resize = photo.resize(resize_size)
-            photo_resize.save(os.path.join(new_dir_name, file))
+        if file == '.DS_Store':continue #Mac特有の隠しファイル対策.
+        photo = Image.open(os.path.join(dir_name, file))
+        photo_resize = photo.resize(resize_size)
+        photo_resize.save(os.path.join(resize_dir, file))
 
 if __name__ == "__main__":
-    resize()
+    resize(6000,4000)
--- hirasawa/photo/UpConvert.py
+++ hirasawa/photo/UpConvert.py
@@ -1,4 +1,4 @@
-#import cv2
+import Resize_DelMeta
 import time
 import os
 import cv2
@@ -9,48 +9,25 @@ import Resize_DelMeta as res_photo
 
 def UpConvert():
     start_time = time.time()
-    # Create an SR object - only function that differs from c++ code
+
     sr = dnn_superres.DnnSuperResImpl_create()
-
-    # Read image
-    image = cv2.imread('./origin/sample.JPG')
-
-    # Read the desired model
+    image = cv2.imread('./origin/sample.jpg')
     path = "EDSR_x4.pb"
     sr.readModel(path)
-
-    # Set the desired model and scale to get correct pre- and post-processing
     sr.setModel("edsr", 4)
-
-    # Upscale the image
     result = sr.upsample(image)
 
-    # Save the image
-    cv2.imwrite("./upconvert/upconvert.JPG", result)
+    cv2.imwrite("./upconvert/upconvert.jpg", result)
     print(time.time()-start_time)
 
-"""
-    dir_name = "origin"
-    os.mkdir("upconvert")
-    save_dir = "upconvert"
-    #original画像のアップコンバート
-
+    Resize_DelMeta.resize(100,100)
     start_time = time.time()
-    # 画像を読み込む
-    files = os.listdir(dir_name)
-    for file in files:
-        if file == '.DS_Store':continue #Mac特有の隠しファイル対策.
-        photo = Image.open(os.path.join(dir_name, file))
-        #img = cv2.imread("image.jpg")
-
-        # SR3アルゴリズムで高画質化を適用する
-        photo_upconvert = SR3(photo, scale=4)
-
-        # 画像を保存する
-        photo_upconvert.save(os.path.join(save_dir, file))
-        #cv2.imwrite(f'result_image_{file}.jpg", photo_upconvert')
+    image = cv2.imread('./resized/sample.jpg')
+    result2 = sr.upsample(image)
+    result3 = sr.upsample(result2)
+    cv2.imwrite("./upconvert/upconvert_resized.jpg", result3)
     print(time.time()-start_time)
-"""
+
 
 if __name__ == "__main__":
     UpConvert()
